9e83e03224ab1cab3662b712ab5af4bbca0d8e8e
ChoiMyungJi <115545101+ChoiMyun@users.noreply.github.com>
2023-02-15 01:35:37 +0900

    0214-2
    new update - for HTML / CSS (section mywork)

--- js/index.js
+++ js/index.js
@@ -36,16 +36,16 @@ $(function(){
   });
 
   // 윈도우 스크롤 이벤트
-  $(window).on('scroll', function(){
-    let scroll = $(this).scrollTop();
+  // $(window).on('scroll', function(){
+  //   let scroll = $(this).scrollTop();
       
-    $('.content').each(function(index){ // <-- 현재 요소의 인덱스번호
-      if( scroll >= pos [index] + base_line && scroll < pos[index+1] + base_line){
-          $(this).addClass('on');
-          $(this).addClass('on').siblings().removeClass('on');
-        }
-    });
-  });
+  //   $('.content').each(function(index){ // <-- 현재 요소의 인덱스번호
+  //     if( scroll >= pos [index] + base_line && scroll < pos[index+1] + base_line){
+  //         $(this).addClass('on');
+  //         $(this).addClass('on').siblings().removeClass('on');
+  //       }
+  //   });
+  // });
     
   // toggle 메뉴바
   $('.toggle').on('click', function(){
@@ -55,25 +55,25 @@ $(function(){
 
 
   // 스와이프
-  let per_view = new Swiper('.per_view .swiper', {
-    slidesPerView: 1,
-    spaceBetween: -30,
-    loop: true,
-    loopAdditionalSlides : 1,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      // type: "fraction",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.per_view .swiper-button-next',
-      prevEl: '.per_view .swiper-button-prev',
-    },
-  });
+  // let per_view = new Swiper('.per_view .swiper', {
+  //   slidesPerView: 1,
+  //   spaceBetween: -30,
+  //   loop: true,
+  //   loopAdditionalSlides : 1,
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     // type: "fraction",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: '.per_view .swiper-button-next',
+  //     prevEl: '.per_view .swiper-button-prev',
+  //   },
+  // });
 
   //about => keyword 소개
   const wr = document.querySelector(".about .left .keyword ul");
@@ -101,58 +101,58 @@ $(function(){
   }
 
    //디자인
-  let count = 0;
-  $(".item").click(function(){
-    $('html,body').on('scroll touchmove mousewheel', function(e) { 
-      e.preventDefault();
-      e.stopPropagation();
-      return false;
-    });
+//   let count = 0;
+//   $(".item").click(function(){
+//     $('html,body').on('scroll touchmove mousewheel', function(e) { 
+//       e.preventDefault();
+//       e.stopPropagation();
+//       return false;
+//     });
 
-    let i = $(this).index();
-    let item_img = $(this).find("img").attr("src");
-    if(i == 3 || i == 5){
-      $(".window-content").css("height", "100%").css("overflow", "hidden scroll")
-    }else{
-      $(".window-content").css("height", "auto").css("overflow", "hidden").css("font-size", "0")
-    }
-    $(".window").slideDown();
-    $(".window-content").slideDown().find("img").attr("src", item_img);
-    $(".arrow").show()
- })
+//     let i = $(this).index();
+//     let item_img = $(this).find("img").attr("src");
+//     if(i == 3 || i == 5){
+//       $(".window-content").css("height", "100%").css("overflow", "hidden scroll")
+//     }else{
+//       $(".window-content").css("height", "auto").css("overflow", "hidden").css("font-size", "0")
+//     }
+//     $(".window").slideDown();
+//     $(".window-content").slideDown().find("img").attr("src", item_img);
+//     $(".arrow").show()
+//  })
 
- $(".arrow i").click(function(){
-  let arrow = $(this).index();
-  if(arrow == 0){
-    count--;
-    if(count < 0){
-      count = $(".item").length - 1;}
-    }else{
-      count++;
-      if(count > $(".item").length-1){
-        count = 0;}
-      }
-      if(count == 3 || count == 5){
-        $(".window-content").css("height", "100%").css("overflow", "hidden scroll")
-      }else{
-        $(".window-content").css("height", "auto").css("font-size", "0").css("overflow", "hidden")
-      }
-      $(".window-content img").attr("src", $(".item").eq(count).find("img").attr("src"))
- });
+//  $(".arrow i").click(function(){
+//   let arrow = $(this).index();
+//   if(arrow == 0){
+//     count--;
+//     if(count < 0){
+//       count = $(".item").length - 1;}
+//     }else{
+//       count++;
+//       if(count > $(".item").length-1){
+//         count = 0;}
+//       }
+//       if(count == 3 || count == 5){
+//         $(".window-content").css("height", "100%").css("overflow", "hidden scroll")
+//       }else{
+//         $(".window-content").css("height", "auto").css("font-size", "0").css("overflow", "hidden")
+//       }
+//       $(".window-content img").attr("src", $(".item").eq(count).find("img").attr("src"))
+//  });
 
- $(".window-content i, .window").click(function(){
-     $(".window").slideUp();
-     $(".window-content, .arrow").slideUp()
-     $('html,body').off('scroll touchmove mousewheel');
- })
+//  $(".window-content i, .window").click(function(){
+//      $(".window").slideUp();
+//      $(".window-content, .arrow").slideUp()
+//      $('html,body').off('scroll touchmove mousewheel');
+//  })
 
 
-  //    // lightbox 
+  //  lightbox  // 
   lightbox.option({
     'resizeDuration': 200,
     'wrapAround': true
   })
-	// //etc탭메뉴
+	// //mywork 탭메뉴
 	$("#mywork .btn p").click(function(){
 		var i = $(this).index()
 		$(".tabBox ul").hide()
@@ -160,10 +160,22 @@ $(function(){
 		return false;
 	})
 
+  const container = document.querySelector(".container");
+  document.body.addEventListener("mousemove", e => {
+  const x = e.clientX;
+  const y = e.clientY - 35;
+  gsap.to(container, {
+    y: y
+  });
+  gsap.to(".menu-mask", {
+    y: -y
+  });
+});
   
 
     
 });
+
 
 
 
