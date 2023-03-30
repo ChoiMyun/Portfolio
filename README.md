702eb015e1ac4b98c42bd89a4765f4c790689790
ChoiMyungJi <115545101+ChoiMyun@users.noreply.github.com>
2023-03-30 22:06:24 +0900

    0330 update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio
- MJ_Portfolio
---
+++ my_porfolio/js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+    //aos 설정
+    AOS.init({easing: 'ease'});
+    
+    //  lightbox  // 
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
+});
+
+
+
+
+
+
+
